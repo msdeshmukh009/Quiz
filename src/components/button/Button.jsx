49b08c0b189dfafc4447c06bd08a4b75c9df3df7
Mahesh <deshmukhmahesh009@gmail.com>
2022-06-25 18:53:16 +0530

    refactor: click handler added

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,6 +1,9 @@
-const Button = ({ children }) => {
+const Button = ({ children, onClickHandler }) => {
   return (
-    <button className="bg-[#FF3B3F] hover:bg-red-500 active:bg-red-700 text-neutral-100 p-2 rounded-[5rem] text-lg font-semibold w-52">
+    <button
+      onClick={onClickHandler}
+      className="bg-[#FF3B3F] hover:bg-red-500 active:bg-red-700 text-neutral-100 p-2 rounded-[5rem] text-lg font-semibold w-52"
+    >
       {children}
     </button>
   );
