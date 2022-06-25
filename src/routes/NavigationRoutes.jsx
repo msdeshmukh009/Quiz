f33393cc585e2d7ab85dd2cf718c997d38b56f7a
Mahesh <deshmukhmahesh009@gmail.com>
2022-06-26 01:10:27 +0530

    feat: question & result routes added

--- src/routes/NavigationRoutes.jsx
+++ src/routes/NavigationRoutes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import { Home } from "../pages";
+import { Home, QuestionPage, ResultPage } from "../pages";
 
 const NavigationRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/question" element={<QuestionPage />} />
+      <Route path="/result" element={<ResultPage />} />
     </Routes>
   );
 };
