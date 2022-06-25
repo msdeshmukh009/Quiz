931a789998e426938d74eff10f4aefb09e6f477b
Mahesh <deshmukhmahesh009@gmail.com>
2022-06-26 01:07:03 +0530

    feat: main functionality done

--- src/components/question/Question.jsx
+++ src/components/question/Question.jsx
@@ -1,8 +1,38 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useQuiz, useTimer } from "../../hooks";
 import { Button } from "../button/Button";
 import { Option } from "../option/Option";
 
-const Question = ({ question }) => {
+const Question = ({ question, setCurrentQuestionIndex, isLastQuestion }) => {
   const { que, options } = question;
+
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const { updateResponses } = useQuiz();
+  const { timeTaken, resetTimer } = useTimer();
+  const navigate = useNavigate();
+
+  const handleOptions = currentOption => {
+    if (selectedOptions.find(option => option.opt === currentOption.opt)) {
+      setSelectedOptions(prevOptions =>
+        prevOptions.filter(option => option.opt !== currentOption.opt)
+      );
+    } else {
+      setSelectedOptions(prevOptions => [...prevOptions, currentOption]);
+    }
+  };
+
+  const nextQuestion = () => {
+    updateResponses({ que, selectedOptions, timeTaken });
+    setCurrentQuestionIndex(prevIndex => prevIndex + 1);
+    resetTimer();
+    setSelectedOptions([]);
+  };
+
+  const submitQuiz = async () => {
+    updateResponses({ que, selectedOptions, timeTaken });
+    navigate("/result");
+  };
 
   return (
     <div className="bg-[#FFFFFF] rounded-xl h-4/5 sm:h-auto p-4 md:w-[65ch] w-full">
@@ -11,13 +41,22 @@ const Question = ({ question }) => {
       </div>
 
       <div className="flex flex-col gap-4 my-4">
-        {options?.map(({ opt }) => (
-          <Option option={opt} key={opt} />
+        {options?.map(option => (
+          <Option
+            option={option}
+            key={option.opt}
+            clickHandler={handleOptions}
+            selectedOptions={selectedOptions}
+          />
         ))}
       </div>
 
       <div className="text-center">
-        <Button>Next</Button>
+        {isLastQuestion ? (
+          <Button onClickHandler={submitQuiz}>Submit</Button>
+        ) : (
+          <Button onClickHandler={nextQuestion}>Next</Button>
+        )}
       </div>
     </div>
   );
