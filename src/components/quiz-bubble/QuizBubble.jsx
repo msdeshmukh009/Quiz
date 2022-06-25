07bea597935619c267d77950789ecba4d1326675
Mahesh <deshmukhmahesh009@gmail.com>
2022-06-26 01:07:55 +0530

    style: margin changed

--- src/components/quiz-bubble/QuizBubble.jsx
+++ src/components/quiz-bubble/QuizBubble.jsx
@@ -1,6 +1,6 @@
 const QuizBubble = ({ children }) => {
   return (
-    <div className="bg-neutral-50 rounded-full w-56 h-56 flex justify-center items-center text-[#FF3B3F] text-4xl font-bold">
+    <div className="bg-neutral-50 my-auto text-center rounded-full w-56 h-56 flex justify-center items-center text-[#FF3B3F] text-3xl p-2 font-bold">
       <h1>{children}</h1>
     </div>
   );
