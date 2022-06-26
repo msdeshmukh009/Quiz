a7ac08664d9bfd4e432c4dbb55194fe606810e18
Mahesh <deshmukhmahesh009@gmail.com>
2022-06-26 13:12:26 +0530

    feat: react speedometer added

--- src/pages/result/ResultPage.jsx
+++ src/pages/result/ResultPage.jsx
@@ -1,3 +1,5 @@
+import ReactSpeedometer from "react-d3-speedometer";
+import * as d3 from "d3";
 import { Button, ResultBox } from "../../components";
 import { useQuiz } from "../../hooks";
 
@@ -21,7 +23,13 @@ const ResultPage = () => {
         ? { ...finalResult, correct: ++finalResult.correct }
         : { ...finalResult, incorrect: ++finalResult.incorrect };
     },
-    { correct: 0, incorrect: 0 }
+    {
+      correct: 0,
+      incorrect: 0,
+      getAccuracy() {
+        return this.correct / (this.correct + this.incorrect);
+      },
+    }
   );
 
   return (
@@ -29,6 +37,22 @@ const ResultPage = () => {
       <div className="bg-[#FFFFFF] rounded-xl h-4/5 sm:h-auto p-4 md:w-[65ch] w-full">
         <div>
           <h1 className="font-semibold text-xl text-center">Your Result</h1>
+        </div>
+
+        <div className="w-full h-80 m-auto">
+          <ReactSpeedometer
+            minValue={0}
+            maxValue={1}
+            endColor="#44B77B"
+            startColor="#FF3B3F"
+            needleColor="#AF9CF3"
+            valueTextFontSize={"32px"}
+            valueTextFontWeight={700}
+            height={150}
+            valueFormat={d3.format(".0%")}
+            value={result.getAccuracy()}
+            fluidWidth={true}
+          />
         </div>
 
         <div className="flex flex-col gap-2 my-2">
