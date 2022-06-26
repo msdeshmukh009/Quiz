2619831d119044b875d4afc1fbb060c7295202be
Mahesh <deshmukhmahesh009@gmail.com>
2022-06-26 17:45:33 +0530

    fix: app breaking after refresh

--- src/components/question/Question.jsx
+++ src/components/question/Question.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useQuiz, useTimer } from "../../hooks";
 import { Button } from "../button/Button";
 import { Option } from "../option/Option";
@@ -12,12 +12,30 @@ const Question = ({
   currentQuestionIndex,
   totalNumberOfQuestion,
 }) => {
-  const { que, options } = question;
+  const destructure = question => {
+    if (question) {
+      const { que, options, questionImage } = question;
+      return { que, options, questionImage };
+    } else {
+      return {
+        que: "",
+        options: [],
+      };
+    }
+  };
+
+  const { que, options, questionImage } = destructure(question);
 
   const [selectedOptions, setSelectedOptions] = useState([]);
   const { updateResponses } = useQuiz();
   const { timeTaken, resetTimer } = useTimer();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!question) {
+      navigate("/");
+    }
+  }, []);
 
   const handleOptions = currentOption => {
     if (selectedOptions.find(option => option.opt === currentOption.opt)) {
@@ -38,11 +56,11 @@ const Question = ({
 
   const submitQuiz = async () => {
     updateResponses({ que, selectedOptions, timeTaken });
-    navigate("/result");
+    navigate("/result", { replace: true });
   };
 
   return (
-    <div className="bg-[#FFFFFF] rounded-xl h-4/5 sm:h-auto p-4 md:w-[65ch] w-full relative">
+    <div className="bg-[#FFFFFF] rounded-xl h-4/5 sm:h-auto p-4 md:w-[65ch] mt-12 w-full relative">
       <div className="mt-[-50px] flex justify-center">
         <ProgressRing
           currentQuestionNumber={currentQuestionIndex + 1}
@@ -50,9 +68,15 @@ const Question = ({
         />
       </div>
 
-      <div className="mt-10">
+      <div className="mt-4">
         <h1 className="font-semibold text-xl">{que}</h1>
       </div>
+
+      {questionImage ? (
+        <div>
+          <img className="w-full max-h-72 my-2" src={questionImage} alt="question-image" />
+        </div>
+      ) : null}
 
       <div className="flex flex-col gap-4 my-4">
         {options?.map(option => (
@@ -67,9 +91,13 @@ const Question = ({
 
       <div className="text-center">
         {isLastQuestion ? (
-          <Button onClickHandler={submitQuiz}>Submit</Button>
+          <Button disableCondition={selectedOptions.length <= 0} onClickHandler={submitQuiz}>
+            Submit
+          </Button>
         ) : (
-          <Button onClickHandler={nextQuestion}>Next</Button>
+          <Button disableCondition={selectedOptions.length <= 0} onClickHandler={nextQuestion}>
+            Next
+          </Button>
         )}
       </div>
     </div>
