7464f196d3389eeb1bacff73bcb3a1026097d786
Mahesh <deshmukhmahesh009@gmail.com>
2022-06-26 17:44:55 +0530

    refactor: disable condition adde

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,8 +1,9 @@
-const Button = ({ children, onClickHandler }) => {
+const Button = ({ children, onClickHandler, disableCondition }) => {
   return (
     <button
       onClick={onClickHandler}
-      className="bg-[#FF3B3F] hover:bg-red-500 active:bg-red-700 text-neutral-100 p-2 rounded-[5rem] text-lg font-semibold w-52"
+      disabled={disableCondition}
+      className="bg-[#FF3B3F] hover:bg-red-500 active:bg-red-700 disabled:cursor-not-allowed text-neutral-100 p-2 rounded-[5rem] text-lg font-semibold w-52"
     >
       {children}
     </button>
